3ae13ebdd59992ff21585d0b3cc3bb5ddb3997e4
TranslationTeam <translate-github@piwigo.org>
2011-09-12 20:02:34 +0000

    [Charlies Content] Update Polski [PL], thanks to : Greybrow
    git-svn-id: http://piwigo.org/svn/extensions/charlies_content@12142 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,59 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2009 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Always'] = 'Zawsze';
+$lang['Autohide'] = 'Autoukrywanie';
+$lang['Autoplay'] = 'Autoodtwarzanie';
+$lang['Curtain selection'] = 'Wybór kurtyny';
+$lang['Default height is out of range (Correct range: 90-1024)'] = 'Domyślna wysokość jest poza zakresem (Prawidłowy zakres: 90-1024)';
+$lang['Default width is out of range (Correct range: 90-1024)'] = 'Domyślna szerokość jest poza zakresem (Prawidłowy zakres: 90-1024)';
+$lang['Display first picture'] = 'Wyświetl pierwszy obraz';
+$lang['Forced height is out of range (Correct range: 90-1024)'] = 'Wymuszona wysokość jest poza zakresem (Prawidłowy zakres: 90-1024)';
+$lang['Forced video height'] = 'Wymuszona wysokość filmu';
+$lang['Forced video width'] = 'Wymuszona szerokość filmu';
+$lang['Forced width is out of range (Correct range: 90-1024)'] = 'Wymuszona szerokość jest poza zakresem (Prawidłowy zakres: 90-1024)';
+$lang['Fullscreen'] = 'Pełny ekran';
+$lang['Keyboard shortcuts'] = 'Skróty klawiszowe';
+$lang['Loop'] = 'Pętla';
+$lang['Never'] = 'Nigdy';
+$lang['Off'] = 'Wyłączone';
+$lang['On'] = 'Włączone';
+$lang['Players colors'] = 'Kolory odtwarzacza';
+$lang['Players options'] = 'Opcje odtwarzacza';
+$lang['Show mouse'] = 'Pokaż kursor myszy';
+$lang['Show stop button'] = 'Pokaż przycisk stop';
+$lang['Show subtitles switch'] = 'Pokaż przełącznik napisów';
+$lang['Show time'] = 'Pokaż czas';
+$lang['Show volume'] = 'Pokaż głośność';
+$lang['Stop stops loading'] = 'Przycisk stop zatrzymuje ładowanie pliku';
+$lang['Templates configuration'] = 'Konfiguracja szablonów';
+$lang['Time left'] = 'Pozostały czas';
+$lang['Unknown file (Start image)'] = 'Nieznany plik (obrazek startowy)';
+$lang['Unknown file or URL|x|y error (x and y must be numeric)'] = 'Nieznany pliku lub URL|x|y błąd (x i y muszą być liczbami)';
+$lang['Video default height'] = 'Domyślna wysokość filmu';
+$lang['Video default width'] = '
+Domyślna szerokość filmu';
+$lang['Volume is out of range (Correct range: 0-200)'] = 'Głośność poza skalą (prawidłowa zawiera się od 0 do 200)';
+$lang['Volume [0-200]'] = 'Głośność [0-200]';
+$lang['_blank'] = '_blank';
+$lang['_self'] = '_self';
+?>

4df10ef727e3432c29dcddfdddbd2087313bc492
TranslationTeam <translate-github@piwigo.org>
2012-05-31 19:40:51 +0000

    [charlie_s_content] Update cs_CZ, thanks to : lanius
    git-svn-id: http://piwigo.org/svn/extensions/charlies_content@15472 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Always'] = 'Vždy';
+$lang['Autoplay'] = 'Autoplay';
+$lang['Background'] = 'Pozadí';
+$lang['Buffer'] = 'Pufr';
+$lang['Margin is out of range (Correct range: 0-40)'] = 'Okraj je mimo rozsah (Oprav rozsah: 0-40)';
+$lang['Never'] = 'Nikdy';
+$lang['Off'] = 'Vypnout';
+$lang['On'] = 'Zapnout';
+$lang['Players colors'] = 'Barvy přehrávače';
+$lang['Players options'] = 'Možnosti přehrávače';
+$lang['Show mouse'] = 'Ukázat myš';
+$lang['Show player'] = 'Ukázat přehravač';
+$lang['Show time'] = 'Ukázat čas';
+$lang['Subtitles'] = 'Podtitulky';
+$lang['Title font size ']['12-72'] = 'Velikost fontu názvu (12-72)';
+$lang['Title'] = 'Název';
+$lang['Unknown file (Start image)'] = 'Neznámý sounor (Počáteční obrázek)';
+$lang['Unknown file (skin)'] = 'Neznámý soubor (skin)';
+?>

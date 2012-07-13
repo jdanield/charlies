3d9dbe3373052309de4b008b167895f4e18eb398
TranslationTeam <translate-github@piwigo.org>
2012-07-13 21:50:17 +0000

    [charlie_s_content] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/charlies_content@16703 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/el_GR/plugin.lang.php
+++ language/el_GR/plugin.lang.php
@@ -23,4 +23,28 @@
 $lang['(Adobe Systems for streaming audio, video and data)'] = '(Adobe Systems για streaming audio, video και δεδομένα)';
 $lang['(URL of a non progressive jpg)'] = '(Το URL ενός μη-προοδευτικού jpg)';
 $lang['(URL|x|y : where x,y are the offsets to place the picture)'] = '(URL | x | y: όπου x, y είναι οι μετατοπίσεις για την τοποθέτηση της  εικόνα)';
+$lang['.png without transparency are recommended. Maybe you can change a set of curtains by yours.'] = '.png χωρίς διαφάνεια συνιστώνται. Ίσως μπορείτε να ανταλλάξετε ένα σύνολο από κουρτίνες με τις δικές σας.';
+$lang['Active curtains'] = 'Ενεργές κουρτίνες';
+$lang['Always'] = 'Πάντοτε';
+$lang['Autohide delay ']['0-9999'] = 'Καθυστέρηση Αυτόματης απόκρυψης [0-9999]';
+$lang['Autohide delay is out of range (Correct range: 0-9999)'] = 'Η Καθυστέρηση Αυτόματης απόκρυψης είναι εκτός της περιοχής (σωστό εύρος: 0-9999)';
+$lang['Autohide'] = 'Αυτόματη απόκρυψη';
+$lang['Autoload'] = 'Αυτόματη Φόρτωση';
+$lang['Autoplay'] = 'Αυτόματη Αναπαραγωγή';
+$lang['Background'] = 'Φόντο';
+$lang['Bind your prefered extensions to a template file (player) from:'] = 'Δεσμεύει τις επεκτάσεις της αρεσκείας σας σε ένα αρχείο προτύπου (player) από:
+';
+$lang['Bottom border fading'] = 'Εξασθένιση των ορίων του κάτω μέρους της οθόνης';
+$lang['Buffer background'] = 'Πλαίσιο ενδιάμεσης μνήμης';
+$lang['Buffer'] = 'Ενδιάμεση μνήμη';
+$lang['Buffering _n_'] = 'αποθήκευση σε προσωρινή μνήμη_n_';
+$lang['Buffering'] = 'αποθήκευση σε προσωρινή μνήμη';
+$lang['Charlies player template-mimetype association'] = 'Πρότυπο Charlies player - σύνδεση Mimetype';
+$lang['Charlies players configuration'] = 'Διαμόρφωση Charlies player';
+$lang['Color usage might be just for information (Players could just ignore them or use them differently)'] = 'Η χρήση χρώματος μπορεί να είναι μόνο για πληροφορίες (Οι Players θα μπορούσαν να τα αγνοήσουν ή απλά να τα χρησιμοποιούν διαφορετικά)';
+$lang['Config by element'] = 'Διαμόρφωση ανά στοιχείο';
+$lang['Curtain selection'] = 'Επιλογή κουρτίνας';
+$lang['Default 1500 milliseconds.'] = 'Προεπιλογή στα 1500 milliseconds.';
+$lang['Default 5 seconds'] = 'Προεπιλογή στα 5 seconds';
+$lang['Default height is out of range (Correct range: 90-1024)'] = 'Προεπιλογή ύψους είναι εκτός εμβέλειας (Σωστό εύρος: 90 έως 1024)';
 ?>

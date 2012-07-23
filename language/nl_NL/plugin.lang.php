23d814675d0a78923116c0953f69568072e09ead
TranslationTeam <translate-github@piwigo.org>
2012-07-23 07:39:49 +0000

    [charlie_s_content] Update nl_NL, thanks to : EdwinKort
    git-svn-id: http://piwigo.org/svn/extensions/charlies_content@16915 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,52 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Active curtains'] = 'Actieve gordijnen';
+$lang['Always'] = 'Altijd';
+$lang['Autohide delay ']['0-9999'] = 'Automatisch verbergen vertraging [0-9999]';
+$lang['Autohide delay is out of range (Correct range: 0-9999)'] = 'Automatisch verbergen vertragen bereik niet geldig (0-9999)';
+$lang['Autohide'] = 'Automatisch verbergen';
+$lang['Autoload'] = 'Automatisch laden';
+$lang['Autoplay'] = 'Automatisch afspelen';
+$lang['Background'] = 'Achtergrond';
+$lang['Buffer background'] = 'Buffer achtergrond';
+$lang['Buffer'] = 'Buffer';
+$lang['Never'] = 'Nooit';
+$lang['Off'] = 'Uit';
+$lang['On'] = 'Aan';
+$lang['PHP streaming'] = 'PHP streaming';
+$lang['Play icon'] = 'Afspeel icoon';
+$lang['Player and Buttons'] = 'Speler en knoppen';
+$lang['Player buttons'] = 'Speler knoppen';
+$lang['Player'] = 'Speler';
+$lang['Players options'] = 'Spelers opties';
+$lang['RTMP server URL'] = 'RTMP server URL';
+$lang['Reset ALL Charlies\' configuration'] = 'Reset ALLE Charlies\' configuratie';
+$lang['Reset ALL Confirmation'] = 'Reset ALLE bevestigingen';
+$lang['Save Charlies\' configuration'] = 'Bewaar configuratie';
+$lang['Show loading'] = 'Toon \'loading\'';
+$lang['Show mouse'] = 'Toon muis';
+$lang['Show player'] = 'Toon speler';
+$lang['Show stop button'] = 'Toon stop knop';
+$lang['_blank'] = '_blank';
+$lang['_self'] = '_self';
+?>

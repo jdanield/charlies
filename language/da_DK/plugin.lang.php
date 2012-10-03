3a9e9270c22a13389151f2603eadc5c2d66cb457
TranslationTeam <translate-github@piwigo.org>
2012-10-03 21:54:03 +0000

    [charlie_s_content] Update da_DK, thanks to : Egorip
    git-svn-id: http://piwigo.org/svn/extensions/charlies_content@18453 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['(Adobe Systems for streaming audio, video and data)'] = '(Adobe Systems til streaming af lyd, video og data)';
+$lang['(URL of a non progressive jpg)'] = '(URL af en ikke progressiv jpg)';
+$lang['(URL|x|y : where x,y are the offsets to place the picture)'] = '(URL|x|y: hvor x, y er den udligning der placere billedet)';
+$lang['.png without transparency are recommended. Maybe you can change a set of curtains by yours.'] = '. png uden gennemsigtighed anbefales. Måske kan du ændre en række gardiner ved din.';
+$lang['Active curtains'] = 'aktive gardiner';
+$lang['Always'] = 'Altid';
+$lang['Autohide'] = 'Auto skjul';
+$lang['Autohide delay ']['0-9999'] = 'Auto skjul forsinkelse [0-9999]';
+$lang['Autohide delay is out of range (Correct range: 0-9999)'] = 'Auto skjul forsinkelse er ude af skalaen (korrekt skala: 0-9999)';
+$lang['Autoload'] = 'Auto indlæs';
+$lang['Autoplay'] = 'Auto afspil';
+$lang['Background'] = 'baggrund';
+$lang['Bind your prefered extensions to a template file (player) from:'] = 'Hæft dine foretrukne udvidelser til en skabelon-fil (afspiller) fra:';
+$lang['Bottom border fading'] = 'Nederste kant falmning';
+$lang['Buffer'] = 'Buffer';
+$lang['Buffer background'] = 'Buffer baggrund';
+$lang['Buffering'] = 'Buffering';
+$lang['Buffering _n_'] = 'Buffering_n_';
+$lang['Charlies player template-mimetype association'] = 'Charlies afspiller skabelon-mimetype associering';
+$lang['Charlies players configuration'] = 'Charlies afspiller konfiguration';
+?>

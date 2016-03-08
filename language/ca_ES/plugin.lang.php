4e5f31f3cc90632ed93fb307c9c55363196c2b23
TranslationTeam <translate-github@piwigo.org>
2016-03-08 21:06:27 +0000

    Add ca_ES, thanks to josep.torne
    git-svn-id: http://piwigo.org/svn/extensions/charlies_content@31413 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Your configuration is NOT saved due to above reasons.'] = 'No s\'ha pogut guardar la configuració degut a diverses raons.';
+$lang['Your configuration is saved.'] = 'S\'ha guardat la configuració.';
+$lang['_blank'] = '_blank';
+$lang['_self'] = '_self';
+$lang['Always'] = 'Sempre';
+$lang['Autohide'] = 'Amaga automàticament';
+$lang['Autohide delay ']['0-9999'] = 'Amaga automàticament despres de [0-9999]';
+$lang['Autoload'] = 'Carrega automàticament';
